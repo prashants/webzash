3c654e0ac696c88872b2b2b2df6267353c8a151d
Prashant Shah <pshah.mumbai@gmail.com>
2014-08-01 13:00:17 +0530

    Add Ledger model method to get diff in opening balance
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

--- Model/Ledger.php
+++ Model/Ledger.php
@@ -205,5 +205,25 @@ class Ledger extends WebzashAppModel {
 			return false;
 		}
 	}
+
+	/* Calculate difference in opening balance */
+	public function getOpeningDiff() {
+		$total_op = 0;
+		$ledgers = $this->find('all');
+		foreach ($ledgers as $row => $ledger)
+		{
+			if ($ledger['Ledger']['op_balance_dc'] == 'D')
+			{
+				$total_op = calculate($total_op, $ledger['Ledger']['op_balance'], '+');
+			} else {
+				$total_op = calculate($total_op, $ledger['Ledger']['op_balance'], '-');
+			}
+		}
+		if (calculate($total_op, 0, '>=')) {
+			return array('opdiff_balance_dc' => 'D', 'opdiff_balance' => $total_op);
+		} else {
+			return array('opdiff_balance_dc' => 'C', 'opdiff_balance' => calculate($total_op, 0, 'n'));
+		}
+	}
 }
 
