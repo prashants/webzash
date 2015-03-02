a1641743bb5a8ea3541f9a78f57708a7b40effa6
Prashant Shah <pshah.mumbai@gmail.com>
2015-03-02 18:02:56 +0530

    Add database unique constrain for code column
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

--- Config/Migrate/Migrate.v2.3.to.v2.4.MySQL.sql
+++ Config/Migrate/Migrate.v2.3.to.v2.4.MySQL.sql
@@ -1,3 +1,5 @@
 /* UPDATE DATABASE TABLES */
 ALTER TABLE `groups` ADD `code` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL AFTER `name`;
+ALTER TABLE `groups` ADD UNIQUE (`code`);
 ALTER TABLE `ledgers` ADD `code` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL AFTER `name` ;
+ALTER TABLE `ledgers` ADD UNIQUE (`code`);
